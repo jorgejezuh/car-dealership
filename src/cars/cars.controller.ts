a7c3237ba60cc8d6825a5353d8b4204ee1362dbe
Jorge Chaparro <jorge@macbook-pro-de-jorge.home>
2023-03-10 21:39:25 +0100

    feat: excepciones y metodos post, patch y delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -19,5 +19,23 @@ export class CarsController {
     getCarById( @Param('id', ParseIntPipe) id: number ){
         console.log({ id })
         return this.carsService.findOneById(+id);
+    }
+
+    @Post()
+    createCar( @Body() payload: any ){
+        return payload
+    }
+
+    @Patch(':id')
+    updateCar( @Body() payload: any ){
+        return payload
+    }
+
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe) id: number ){
+        return {
+            method: 'DELETE',
+            id
+        }
     }
 }
